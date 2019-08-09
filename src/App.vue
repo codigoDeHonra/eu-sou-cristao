11a29c7d3e2faf29323a2c683d393c0ce16f2fda
wouerner <wouerner@protonmail.com>
2019-08-09 10:36:06 -0300

    Paginas Iniciais

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
         <span>Eu sou Cristão</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items>
+          <v-btn text>Cadastro</v-btn>
+          <v-btn text>Lista</v-btn>
+          <v-btn text>Sobre</v-btn>
+          <v-btn text>Privaciade</v-btn>
+          <v-btn text>Como ajudar?</v-btn>
+          <v-btn text>Perguntas?</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-content>
